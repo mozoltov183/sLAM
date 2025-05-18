a9ece1c1d4922ac6e615cede70e87fa817631f1d
Mozoltov <63779119+mozoltov183@users.noreply.github.com>
2025-05-18 20:51:44 +0800

    Add files via upload

---
+++ prompts/prompt_manager.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+import frontmatter
+from jinja2 import Environment, FileSystemLoader, StrictUndefined, TemplateError, meta
+import ipdb
+
+class PromptManager:
+    _env =None
+
+    @classmethod
+    def _get_env(cls, templates_dir="prompts/templates"):
+        templates_dir = Path(__file__).parent.parent/templates_dir
+        if cls._env is None:
+            cls._env = Environment(
+                loader = FileSystemLoader(templates_dir),
+                undefined = StrictUndefined,
+            )
+        return cls._env
+
+    @staticmethod
+    def get_prompt(template, **kwargs):
+        # ipdb.set_trace()
+        env = PromptManager._get_env()
+        template_path = f"{template}.j2"
+        with open(env.loader.get_source(env, template_path)[1], "r", encoding="utf-8") as fp:
+            post = frontmatter.loads(fp.read())
+
+        template = env.from_string(post.content)
+        try:
+            return template.render(**kwargs)
+        except TemplateError as e:
+            raise ValueError(f"Error rendering template:{str(e)}")
+
+    @staticmethod
+    def get_template_info(template):
+        env = PromptManager._get_env()
+        template_path = f"{template}.j2"
+        with open(env.loader.get_source(env, template_path)[1], "r", encoding="utf-8") as fp:
+            # post = frontmatter.loads(fp.read())
+            post = frontmatter.loads(fp.read())
+
+        ast = env.parse(post.content)
+        variables = meta.find_undeclared_variables(ast)
+        return {
+            "name": template,
+            "description": post.metadata.get("description", "No description provided"),
+            "author": post.metadata.get("author", "Unknown"),
+            "variables": list(variables),
+            "frontmatter": post.metadata,
+        }
